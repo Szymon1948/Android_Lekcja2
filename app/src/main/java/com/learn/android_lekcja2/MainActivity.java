103c9aa458ba6f1c3c160eee8091bb19c5ab4ddf
Szymon1948 <szymonintrys@gmail.com>
2022-10-13 15:59:19 +0200

    Poprawne odpowiedzi

--- app/src/main/java/com/learn/android_lekcja2/MainActivity.java
+++ app/src/main/java/com/learn/android_lekcja2/MainActivity.java
@@ -4,26 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Pytanie> pytanie = new ArrayList<>();
+    TextView pytTextView;
+    int licznik;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        pytTextView = findViewById(R.id.pytanieTextView);
+        dodajPytania();
+        wyswietlPytanie(0);
+    }
+    private void wyswietlPytanie(int i){
+        pytTextView.setText(pytanie.get(i).getTresc());
+    }
+    private void dodajPytania(){
+        pytanie.add(new Pytanie("Jezeli na ciało nie działa żadna siła," +
+                "ciało porusza się ruchem jednostajnym",true) );
+        pytanie.add(new Pytanie("ciało spada swobodnie z przyśpieszeniem 20m/s", false));
+        pytanie.add(new Pytanie("Czy ziemia jest płaska ?",false));
+        pytanie.add(new Pytanie("W Autobusie podczas hamowania działa siła " +
+                "bezwładnosći w kierunku jazdy autobusu",true));
     }
     public void sprawdz(View view){
         Integer id = view.getId();
-        Toast.makeText(MainActivity.this,
-                id.toString(),
-                Toast.LENGTH_SHORT).show();
-        if(id == R.id.button4){
-            Toast.makeText(MainActivity.this, "Dobra odpowiedź :)", Toast.LENGTH_SHORT).show();
+        if(pytanie.get(licznik).isOdpowiedz() ) {
+            if (id == R.id.button4) {
+                Toast.makeText(MainActivity.this, "Dobra odpowiedź :)", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Zła odpowiedź :/", Toast.LENGTH_SHORT).show();
+            }
+        }
+       else{
+            if (id == R.id.button4) {
+                Toast.makeText(MainActivity.this, "Zła odpowiedź :/", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Dobra odpowiedź :)", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    public void kolejne(View view) {
+        licznik++;
+        if(licznik==pytanie.size()){
+            pytTextView.setText("Koniec Testu");
+            view.setVisibility(View.INVISIBLE);
         }
         else{
-            Toast.makeText(MainActivity.this, "Zła odpowiedź :/", Toast.LENGTH_SHORT).show();
+            wyswietlPytanie(licznik);
         }
+
     }
 }
---
+++ app/src/main/java/com/learn/android_lekcja2/Pytanie.java
@@ -0,0 +1,48 @@
+package com.learn.android_lekcja2;
+
+public class Pytanie {
+    private String tresc;
+    private boolean odpowiedz;
+    private static int licznik;
+    private int nr;
+
+    public Pytanie(String tresc, boolean odpowiedz) {
+        licznik++;
+        this.tresc = tresc;
+        this.odpowiedz = odpowiedz;
+        this.nr = licznik;
+
+    }
+
+    public String getTresc() {
+        return tresc;
+    }
+
+    public void setTresc(String tresc) {
+        this.tresc = tresc;
+    }
+
+    public boolean isOdpowiedz() {
+        return odpowiedz;
+    }
+
+    public void setOdpowiedz(boolean odpowiedz) {
+        this.odpowiedz = odpowiedz;
+    }
+
+    public static int getLicznik() {
+        return licznik;
+    }
+
+    public static void setLicznik(int licznik) {
+        Pytanie.licznik = licznik;
+    }
+
+    public int getNr() {
+        return nr;
+    }
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+}
